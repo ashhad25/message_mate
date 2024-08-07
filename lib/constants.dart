b4cf751e64cec50da40c067ffc34ae9805b254fd
Ashhad Ahmed <81470672+ashhad25@users.noreply.github.com>
2024-08-07 16:19:01 +0500

    color constants

---
+++ lib/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor appBarColor = dark;
+const Color textColor = Colors.white;
+const Color titleColor = Color(0xFF25d366);
+
+const MaterialColor dark = MaterialColor(
+  0xFF0A0A0A,
+  <int, Color>{
+    50: Color(0xFF0A0A0A),
+    100: Color(0xFF0A0A0A),
+    200: Color(0xFF0A0A0A),
+    300: Color(0xFF0A0A0A),
+    400: Color(0xFF0A0A0A),
+    500: Color(0xFF0A0A0A),
+    600: Color(0xFF0A0A0A),
+    700: Color(0xFF0A0A0A),
+    800: Color(0xFF0A0A0A),
+    900: Color(0xFF0A0A0A),
+  },
+);
+const MaterialColor light = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
